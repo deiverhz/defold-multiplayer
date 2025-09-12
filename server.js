72150f8e7bf800c93614d6f7ecd1a073a5cd89da
deiverhs <deiverhs@outlook.com>
2025-09-11 18:14:34 -0600

    Set WebSocket server to use port 3000
    Changed WebSocket server port to a fixed value of 3000.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
+const wss = new WebSocket.Server({ port: 3000 });
 
 wss.on("connection", ws => {
   ws.on("message", message => {
